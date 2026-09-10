e9caef3395bfb21767ac98c85939ed3888470152
Amaney Ibrahim <ibrahim.amaney@student.ccm.edu>
2026-09-10 11:03:24 -0400

    I haven't done any changes

---
+++ character_profile.py
@@ -0,0 +1,9 @@
+# Student name: Amaney Ibrahim
+# Course number: CMP 131
+# Week number: 3
+# Lab number: 1 
+# Assignment title: Character profile and Favorite quote 
+# Date: 9/10/2026
+name="Character Profile: Rory Gilmore"
+Age="16"
+Occupation=""
