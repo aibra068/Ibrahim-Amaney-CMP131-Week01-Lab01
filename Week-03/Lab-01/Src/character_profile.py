8c4b7b4560395ad496f644a84131000dff27ac8d
Amaney Ibrahim <ibrahim.amaney@student.ccm.edu>
2026-09-17 16:38:21 -0400

    Complete Week 3 Lab 1

---
+++ Week-03/Lab-01/Src/character_profile.py
@@ -0,0 +1,27 @@
+# Student name: Amaney Ibrahim
+# Course number: CMP 131
+# Week number: 3
+# Lab number: 1 
+# Assignment title: Character profile and Favorite quote 
+# Date: 9/10/2026
+print("=============================================================")
+print("            ^^Gilmore Girls Character Profile^^              ")
+print("=============================================================")
+print()
+print("------------------^^Charatcter Info^^------------------------")
+print()
+print("Name: Lorelai Gilmore")
+print("Age: 32")
+print("Occupation: Hotel Manager")
+print()
+print("------------^^Character Additional Attributes^^--------------")
+print()
+print("~ Important accomplishment ~ Owning her own inn")
+print("~ Interesting fact ~ She can smell when it is about to snow")
+print("~ Favorite food ~ Pizza")
+print("~ Personality ~ Funny and Whitty")
+print()
+print("----------------^^Favorite Quote^^---------------------------")
+print()                                                                                                     
+print('" Everything in my life has something to do with coffee. I believe in a former life I was coffe."')
+print("~ Lorelai Gilmore")
